72fe78fe7e434c76d8654fcc7e416dfd0b02d04d
DevonTheFloor <devonthefloor@outlook.fr>
2024-02-13 19:45:10 +0100

    Band and Listings models ok

--- firstone/listings/views.py
+++ firstone/listings/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
 
 def hello(request):
-    return HttpResponse('<h1>Hello Maestro Django!</h1>')
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html',{'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
---
+++ trash.md
@@ -0,0 +1,1 @@
+        <!--<link rel="stylesheet" href="{% static 'listings/styles.css' %}" />-->
